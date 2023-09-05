fd72e4c4bfaea45b7ba8dc33dd76bd67c745de68
A01710217 <111156446+A01710217@users.noreply.github.com>
2023-09-04 23:29:07 -0600

    funciones.h

---
+++ Tareas/Tarea1/funciones.h
@@ -0,0 +1,37 @@
+// =========================================================
+// Nombre: funciones.h
+// Autor: Axel Camacho Villafuerte.
+// Fecha: 25/08/2023.
+// =========================================================
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+
+class Funciones {
+    public:
+    int sumaIterativa(int n);
+    int sumaRecursiva(int n);
+    int sumaDirecta(int n);
+};
+
+int Funciones::sumaIterativa(int n) {
+	int cont = 0;
+	for (int i = 0; n >= i; i++) {
+		cont += i;
+	}
+	return cont;
+}
+
+int Funciones::sumaRecursiva(int n) {
+    if (n == 0) return	0;
+	else return n + sumaRecursiva(n-1);
+}
+
+int Funciones::sumaDirecta(int n) {
+    int sumD = (n * (n + 1)) / 2;
+    return sumD;
+}
+
+#endif
